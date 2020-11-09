b42278995a440532292be9748417628aa49dd202
eciccone <ecicc2@uis.edu>
2020-11-08 20:51:49 -0600

    implemented BalaAnt functionality

--- src/simulation/model/BalaAnt.java
+++ src/simulation/model/BalaAnt.java
@@ -1,6 +1,11 @@
 package simulation.model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.Stack;
+
+import simulation.util.Moves;
 
 public class BalaAnt extends MoveableAnt implements ActionableAnt {
 
@@ -10,7 +15,60 @@ public class BalaAnt extends MoveableAnt implements ActionableAnt {
 
 	@Override
 	public void performAction(ColonyNode[][] grid, LinkedList<Ant> ants) {
+		ColonyNode node = grid[getLocation().getX()][getLocation().getY()];
 		
+		if(enemiesInRange(node)) {
+			attackEnemy(grid, ants);
+		} else {
+			move(grid, Moves.getUnrestrictedMoves(getLocation()));
+		}
 	}
 
+	private void attackEnemy(ColonyNode[][] grid, LinkedList<Ant> ants) {
+		LinkedList<Ant> possibleTargets = new LinkedList<Ant>();
+		
+		for(Ant ant : ants) {
+			if(isAttackableEnemy(ant) && ant.isAlive())
+				possibleTargets.add(ant);
+		}
+		
+		if(possibleTargets.size() > 0) {
+			Ant antToAttack = possibleTargets.get(new Random().nextInt(possibleTargets.size()));
+			
+			// 50% chance of successfully killing enemy.
+			if(new Random().nextBoolean()) {
+				antToAttack.setAlive(false);
+				
+				// Handle if ForagerAnt was carrying food
+				if(antToAttack instanceof ForagerAnt) {
+					if(((ForagerAnt) antToAttack).isCarryingFood()) {
+						ColonyNode node = grid[antToAttack.getLocation().getX()][antToAttack.getLocation().getY()];
+						node.setFoodAmount(node.getFoodAmount() + 1);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Determines if any enemies of this ant are in the given node.
+	 * 
+	 * @param node The ColonyNode to check if there are any enemies
+	 * @return true if any enemy ants are present, otherwise false
+	 */
+	private boolean enemiesInRange(ColonyNode node) {
+		return node.getScoutCount() > 0 || node.getSoldierCount() > 0 || node.getForagerCount() > 0
+				|| node.isQueenPresent();
+	}
+	/**
+	 * Determines if a given ant is allowed to be attacked.
+	 * 
+	 * @param ant The Ant to check if it can be attacked
+	 * @return true if the ant is attackable, false otherwise
+	 */
+	private boolean isAttackableEnemy(Ant ant) {
+		return ant.getLocation().getX() == getLocation().getX() 
+				&& ant.getLocation().getY() == getLocation().getY() 
+				&& !(ant instanceof BalaAnt);
+	}
 }

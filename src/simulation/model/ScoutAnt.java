40f4a162e46e6c528f01f2b85eae863ff8c10791
eciccone <ecicc2@uis.edu>
2020-11-08 20:05:43 -0600

    implemented ScoutAnt functionality

--- src/simulation/model/ScoutAnt.java
+++ src/simulation/model/ScoutAnt.java
@@ -2,6 +2,8 @@ package simulation.model;
 
 import java.util.LinkedList;
 
+import simulation.util.Moves;
+
 public class ScoutAnt extends MoveableAnt implements ActionableAnt {
 
 	public ScoutAnt(int id, Location location) {
@@ -10,7 +12,19 @@ public class ScoutAnt extends MoveableAnt implements ActionableAnt {
 
 	@Override
 	public void performAction(ColonyNode[][] grid, LinkedList<Ant> ants) {
+		move(grid, Moves.getUnrestrictedMoves(getLocation()));
 		
+		ColonyNode node = grid[getLocation().getX()][getLocation().getY()];
+		
+		if(!node.isDiscovered()) {
+			node.setDiscovered(true);
+			randomlyGenerateFood(node);
+		}
+	}
+
+	private void randomlyGenerateFood(ColonyNode node) {
+		if(Ant.random().nextInt(100) + 1 <= 25)
+			node.setFoodAmount(Ant.random().nextInt(1000 - 500 + 1) + 500);
 	}
 
 }

83c072bb1977f9b8e9d9ad91c32e3ab6492a04f8
eciccone <ecicc2@uis.edu>
2020-11-08 19:41:55 -0600

    implemented queen functionality

--- src/simulation/model/QueenAnt.java
+++ src/simulation/model/QueenAnt.java
@@ -6,4 +6,23 @@ public class QueenAnt extends Ant {
 		super(id, location);
 	}
 	
+	public Ant hatchAnt(int antCount) {
+		int randomNumber = Ant.random().nextInt(100) + 1;
+		
+		if(randomNumber <= 40)
+			return new ForagerAnt(antCount, new Location(13, 13));
+		else if(randomNumber <= 50)
+			return new ScoutAnt(antCount, new Location(13, 13));
+		else
+			return new SoldierAnt(antCount, new Location(13, 13));
+	}
+	
+	public boolean eat(ColonyNode colonyNode) {
+		if(colonyNode.getFoodAmount() == 0)
+			return false;	
+		int foodAmountAfterEating = colonyNode.getFoodAmount() - 1;
+		colonyNode.setFoodAmount(foodAmountAfterEating);
+		return true;
+	}
+	
 }

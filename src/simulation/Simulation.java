f89c8f737ed2e6cc3a191b9ea161fadec6f7cc23
eciccone <ecicc2@uis.edu>
2020-11-08 19:37:53 -0600

    implemented GUI functionality

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -1,5 +1,78 @@
 package simulation;
 
-public class Simulation {
+import simulation.model.Colony;
+import simulation.view.AntSimGUI;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+public class Simulation implements SimulationEventListener, ActionListener {
+
+    private int turns;
+    private Colony colony;
+    private AntSimGUI antSimGUI;
+    private boolean initializedColony;
+    private Timer timer;
+    
+    public Simulation() {
+        turns = 0;
+        colony = new Colony();
+        antSimGUI = new AntSimGUI();
+        initializedColony = false;
+        timer = new Timer(2, this);
+        antSimGUI.addSimulationEventListener(this);
+        antSimGUI.initGUI(colony.getColonyView());
+    }
+    
+    public void step() {
+    	turns++;
+    	colony.update(turns);
+    	System.out.println("Update colony --- Turn Count: " + turns);
+    }
+
+    @Override
+    public void simulationEventOccurred(SimulationEvent simEvent) {
+        if(simEvent.getEventType() == SimulationEvent.NORMAL_SETUP_EVENT) {
+        	if(timer.isRunning()) {
+        		timer.stop();
+        	}
+        	
+        	if(initializedColony) {
+        		colony.reset();
+        		turns = 0;
+        	}
+        	
+        	colony.normalSetup();
+        	initializedColony = true;
+        } else if(simEvent.getEventType() == SimulationEvent.RUN_EVENT) {
+        	if(initializedColony) {
+        		if(timer.isRunning()) {
+        			timer.stop();
+        		} else {
+        			timer.start();
+        		}
+        	}
+        } else if(simEvent.getEventType() == SimulationEvent.STEP_EVENT) {
+        	if(initializedColony && !timer.isRunning()) {
+        		if(colony.getQueenAnt().isAlive()) {
+        			step();
+        		} else {
+        			System.out.println("Queen is dead --- Cannot step.");
+        		}
+        	}
+        }
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(colony.getQueenAnt().isAlive()) {
+			step();
+		} else {
+			timer.stop();
+			System.out.println("Queen is dead --- Cannot run.");
+		}
+	}
+	
 }
